fc94d8277dad02523b9954f1b92ac0c41b1aa174
Arshad Antu <arshad.antu@gmail.com>
2026-03-01 05:08:04 +0800

    added remove_element

--- Problems/remove_element/remove_element.go
+++ Problems/remove_element/remove_element.go
@@ -1,5 +1,12 @@
 package remove_element
 
-func removeElement() {
-	// TODO: implement
+func removeElement(nums []int, val int) int {
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
 }
--- Problems/remove_element/remove_element_test.go
+++ Problems/remove_element/remove_element_test.go
@@ -3,5 +3,18 @@ package remove_element
 import "testing"
 
 func TestRemoveElement(t *testing.T) {
-	t.Fatal("not implemented")
+	tests := []struct {
+		nums     []int
+		val      int
+		expected int
+	}{
+		{[]int{3, 2, 2, 3}, 3, 2},
+		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5},
+	}
+	for _, test := range tests {
+		result := removeElement(test.nums, test.val)
+		if result != test.expected {
+			t.Errorf("removeElement(%v, %d) = %d; expected %d", test.nums, test.val, result, test.expected)
+		}
+	}
 }
